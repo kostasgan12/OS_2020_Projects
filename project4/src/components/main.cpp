2646f5fb6aff48f14e3ef7ded8c1eb5e4986cb52
Konstantinos <konstantinos.ganetsos@gmail.com>
2021-01-25 01:09:32 +0200

    🎉

--- project4/src/components/main.cpp
+++ project4/src/components/main.cpp
@@ -13,30 +13,33 @@ int main(int argc, char *argv[])
     if (argc > 1 && string(argv[1]) == "-v")
     { //verbose option, if it exist then we print program procedure info
         verbose = 1;
+        cout << "\tVerbose Mode: ON"<< endl;
     }
     else
     {
-        cout << "########### Unrecognised Verbose Flag ###########\n"<< endl;
+        cout << "\tVerbose Mode: OFF"<< endl;
     }
 
     if (argc > 2 && string(argv[2]) == "-d")
     {   //deleted option, if it exist then,
         //we should make sure deleted files in A get deleted in B
         verifyDeleted = 1;
+         cout << "\tVerify Deletion Mode: ON"<< endl;
     }
     else
     {
-        cout << "########### Unrecognised Verify Deletion Flag ###########\n"<< endl;
+        cout << "\tVerify Deletion Mode: OFF"<< endl;
     }
 
     if (argc > 3 && string(argv[3]) == "-l")
     {   //consider links option, if it exist then,
         //our program should consider possible links
         considerLinks = 1;
+        cout << "\tConsider Links Mode: ON"<< endl;
     }
     else
     {
-        cout << "########### Unrecognised Consider Links Flag ###########\n"<< endl;
+        cout << "\tConsider Links Mode: OFF"<< endl;
     }
 
     //////////////////////////////////////////////////////////////////////////////////////
